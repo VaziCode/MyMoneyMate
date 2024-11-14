576ce136b1a7e04ad85947aa31bffbceea57de3f
VaziCode <talva@ac.sce.ac.il>
2024-11-14 20:31:43 +0200

    adding running file

---
+++ run_app.bat
@@ -0,0 +1,7 @@
+@echo off
+python Backend\main.py ^
+    --db_host "your_db_host" ^
+    --db_name "your_db_name" ^
+    --db_user "your_db_user" ^
+    --db_password "your_password" ^
+    --db_port your_port
